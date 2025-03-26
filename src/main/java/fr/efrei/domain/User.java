d1e2bdcac6710c24aa16dd27e3af79e24fdb58a5
219099405 <219099405@mycput.ac.za>
2025-03-26 11:36:20 +0200

    Basecode

---
+++ src/main/java/fr/efrei/domain/User.java
@@ -0,0 +1,72 @@
+package fr.efrei.domain;
+
+import java.lang.module.ModuleDescriptor;
+
+public class User {
+    private int userId;
+    private String userName;
+    private String email;
+    private String password;
+
+
+
+    private User(){}
+
+    private User(Builder builder){
+        this.userId = builder.userId;
+        this.userName = builder.userName;
+        this.email = builder.email;
+        this.password = builder.password;
+    }
+        public int getUserId() {
+            return userId;
+        }
+
+    public String getUserName() {
+        return userName;
+    }
+    public String getEmail() {
+        return email;
+
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private int userId;
+        private String userName;
+        private String email;
+        private String password;
+
+        public Builder setUserId(int userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder setUserName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+
+        }
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+        public User build() { return new User(this);}
+    }
+}
